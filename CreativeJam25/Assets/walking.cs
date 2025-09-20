e9f9595434584fd209abdf71467f0f5f0ebd9b4e
AdamFarahat <farahatadam@gmail.com>
2025-09-20 10:31:14 -0400

    Put Rigidbody movement to FixedUpdate, change var to Vector3D

--- CreativeJam25/Assets/walking.cs
+++ CreativeJam25/Assets/walking.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil.Cil;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -5,26 +6,25 @@ using UnityEngine;
 
 public class walkingIdkIWillRenameItLater : MonoBehaviour
 {
-    public Rigidbody2D rigid; // the rigid body attached to the player
-    [SerializeField] private int speed; //how fast the plr moves. Feel free to turn it to a float if you want idc. Idk how fast or slow you want the guy to move
+    public Rigidbody2D rigid { get; private set; } // the rigid body attached to the player
 
+    Vector3 velocityVector; // the direction the plr is moving in. This is a unit vector so it only gives direction, not speed
+    [SerializeField] private int speed; //how fast the plr moves. Feel free to turn it to a float if you want idc. Idk how fast or slow you want the guy to move
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>(); // gets the rigid body component
+    }
     void Update()
     {
-        
-        if (Input.GetMouseButton(0)) // if LMB is held down
-        {
-            
-            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // position of mouse
-            var plrPos = transform.position; // position of plr
-            mousePos.z = plrPos.z; // make the z's equal because for some fucking reason we need to worry about z in a 2D environment
-            var velocityVector = (mousePos - plrPos).normalized; // turns it into a unit vector so movement doesnt change depending on how far mouse is from plr
-            rigid.linearVelocity = velocityVector * speed; // sets the velocity, tweak speed in the properties of the script
-            
-        }
-        
-        
-        
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // position of mouse
+        var plrPos = transform.position; // position of plr
+        mousePos.z = plrPos.z; // make the z's equal because for some fucking reason we need to worry about z in a 2D environment
+        velocityVector = (mousePos - plrPos).normalized; // turns it into a unit vector so movement doesnt change depending on how far mouse is from plr
+    }
 
+    void FixedUpdate()
+    {
+        rigid.linearVelocity = velocityVector * speed; // sets the velocity, tweak speed in the properties of the script
     }
 }
 
