411b350da5d5b2f40d66060ba1a400f17bdb1090
AdamFarahat <farahatadam@gmail.com>
2025-09-20 16:50:57 -0400

    Implement sprite blinking and invincibility frames

--- CreativeJam25/Assets/Scripts/Player/PlayerStats.cs
+++ CreativeJam25/Assets/Scripts/Player/PlayerStats.cs
@@ -1,5 +1,7 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -10,43 +12,89 @@ public class PlayerStats : MonoBehaviour
 
     public float voidRadius;
 
+    SpriteRenderer spriteRenderer; //reference to the sprite renderer component
+
+    [SerializeField] private float invincibilityDuration = 2.0f; //how long the plr is invincible for after taking damage
+
+    private float blinkDuration; //how fast the plr blinks when invincible
+
+    [SerializeField] private float blinkCooldown = 0.5f;
+
     RaycastHit2D voidAura;
     UnityEvent OnHealthChanged = new UnityEvent(); //event that triggers when health changes
+
+    UnityEvent OnTakeDamage = new UnityEvent(); //event that triggers when the player takes damage
+
+    UnityEvent OnDeath = new UnityEvent(); //event that triggers when the player dies
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         health = maxHealth;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         voidAura = Physics2D.CircleCast(transform.position, voidRadius, Vector2.zero, 0);
-        if (voidAura.collider != null)
+
+        //When I click spacebar, take 10 damage
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            TakeDamage(0.1f);
+            Debug.Log("Spacebar pressed, taking 10 damage");
+            TakeDamage(10.0f);
+        }
+
+        if (PlayerState.instance.currentState == PlayerState.PlayerStates.Damaged)
+        {
+            if (blinkDuration > 0.0f)
+            {
+                blinkDuration -= Time.deltaTime;
+            }
+            else
+            {
+                spriteRenderer.enabled = !spriteRenderer.enabled;
+                blinkDuration = blinkCooldown;
+            }
+        }
+        else
+        {
+            spriteRenderer.enabled = true;
         }
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
-        Gizmos.DrawWireSphere(transform.position, voidRadius);   
+        Gizmos.DrawWireSphere(transform.position, voidRadius);
     }
 
     public void TakeDamage(float damage)
     {
+        Debug.Log("Taking damage: " + damage);
+        if (PlayerState.instance.currentState == PlayerState.PlayerStates.Dead ||
+        PlayerState.instance.currentState == PlayerState.PlayerStates.Damaged)
+        {
+            Debug.Log("Player is invincible, not taking damage, or dead");
+            return;
+        }
         health -= damage;
         OnHealthChanged.Invoke();
-        /*if (health <= 0)
+        if (health <= 0)
         {
+            health = 0;
             PlayerState.instance.currentState = PlayerState.PlayerStates.Dead;
+            Debug.Log("Player died");
+            OnDeath.Invoke();
             //trigger death event
         }
         else
         {
             PlayerState.instance.currentState = PlayerState.PlayerStates.Damaged;
+            Debug.Log("Player took damage, health is now: " + health);
+            StartCoroutine(InvicibilityFrames());
+            OnTakeDamage.Invoke();
             //trigger damaged event
-        }*/
+        }
     }
 
     public void IncreaseMaxHealth(float amount)
@@ -65,5 +113,11 @@ public class PlayerStats : MonoBehaviour
     {
         //increase void aura radius
         voidRadius *= 1 + (amount / 100);
+    }
+    private IEnumerator InvicibilityFrames()
+    {
+        blinkDuration = blinkCooldown;
+        yield return new WaitForSeconds(invincibilityDuration);
+        PlayerState.instance.currentState = PlayerState.PlayerStates.Normal;
     }
 }
