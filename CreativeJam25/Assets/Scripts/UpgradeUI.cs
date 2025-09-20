d77ca715de7aeabe134a125f8681cf4c9ecbe7e4
AdamFarahat <farahatadam@gmail.com>
2025-09-20 13:15:37 -0400

    Stat Upgrade type implemented (not connected)

--- CreativeJam25/Assets/Scripts/UpgradeUI.cs
+++ CreativeJam25/Assets/Scripts/UpgradeUI.cs
@@ -1,20 +1,34 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class UpgradeUI : MonoBehaviour
 {
     public GameObject[] upgradePanels = new GameObject[3];
 
+    [SerializeField] private Upgrade[] upgrades; // Array to hold the upgrades
+    
+    public List<Upgrade> unSelectedUpgrades = new List<Upgrade>();
+
     public void Start()
     {
         UpgradeManager.Instance.OnLevelUp.AddListener(OpenUpgradeMenu);
+        for (int i = 0; i < upgradePanels.Length; i++)
+        {
+            upgradePanels[i].GetComponent<UpgradePanel>().GetComponent<Button>().onClick.AddListener(CloseUpgradeMenu);
+        }
         gameObject.SetActive(false);
     }
 
     public void OpenUpgradeMenu()
     {
         gameObject.SetActive(true);
+        //Reset the unSelected upgrades
+        for(int i=0; i < upgrades.Length; i++)
+        {
+            unSelectedUpgrades.Add(upgrades[i]);
+        }
         //Change gamestart to InGameMenu state
         UpgradeRandomizer();
     }
@@ -23,23 +37,26 @@ public class UpgradeUI : MonoBehaviour
     {
         //Load all upgrades from Resoures/Upgrades
         //There are 2 types of upgrades: Weapon and Ability
-        //GameObject[] allUpgrades = Resources.LoadAll<GameObject>("Upgrades");
         //Randomly select upgrades equal to the size of upgradePanels from the list of all upgrades
         for (int i = 0; i < upgradePanels.Length; i++)
         {
-            TextMeshProUGUI title = upgradePanels[i].transform.Find("Title").GetComponent<TextMeshProUGUI>();
-            TextMeshProUGUI description = upgradePanels[i].transform.Find("Description").GetComponent<TextMeshProUGUI>();
-            title.text = "Upgrade " + (i + 1);
-            description.text = "This is a description of upgrade " + (i + 1);
-            //Attach the upgrade to the panel (Assume the upgrade is a prefab with a script called Upgrade that has a method called ApplyUpgrade)
+            int upgradeIndex = Random.Range(0, unSelectedUpgrades.Count);
+            Upgrade randomUpgrade = unSelectedUpgrades[upgradeIndex];
+            //Attach the upgrade to the panel (Assume the upgrade is a prefab with a script called Upgrade that has a method called ApplyUpgrade).
+            //Change the Description to fit the upgrade description
+            //If it is a stat upgrade, have description say "Increases [stat] by [amount]%"
+            if (randomUpgrade is StatUpgrade statUpgrade)
+            {
+                statUpgrade.UpgradeAmount = Random.Range(1, 100); // Random amount between 5% and 20%
+                randomUpgrade.Description = "Increases " + statUpgrade.GetType().Name + " by " + statUpgrade.UpgradeAmount + "%";
+            }
+                upgradePanels[i].GetComponent<UpgradePanel>().SetUpgrade(randomUpgrade);
+            unSelectedUpgrades.RemoveAt(upgradeIndex);
         }
     }
 
     public void CloseUpgradeMenu()
     {
-        //Which button was pressed?
-        //Apply the upgrade to the player.
-        //Close the menu and resume the game.
         gameObject.SetActive(false);
         Time.timeScale = 1f; // Resume the game
         //Change gamestate back to Level
