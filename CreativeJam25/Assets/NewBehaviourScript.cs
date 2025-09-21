75d018aa73ff91752fc5061e34329f044c134882
PickledPeanuts <h.torch250@gmail.com>
2025-09-20 21:24:31 -0400

    finished the tile system

--- CreativeJam25/Assets/NewBehaviourScript.cs
+++ CreativeJam25/Assets/NewBehaviourScript.cs
@@ -9,65 +9,69 @@ public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] private Tilemap tileset;
     [SerializeField] private Rigidbody2D plr;
+    [SerializeField] private UpgradeManager uManager;
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        var a = new Vector3[1];
-        var b = new int[1];
 
-        a[0] = plr.position;
-        b[0] = 0;
-        //print(plr.position);
-        //print(a[0]);
 
-        placeTiles(a, b);
 
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var a = new Vector3[1]; //testing purposes
+
+        a[0] = plr.position;
+
+        placeTiles(a, 0); 
+        print(isColoured(a[0]));
     }
 
-    bool isColoured(Vector3 Position)
+    bool isColoured(Vector3 Position) //checks whether the tile at the position inputted is coloured. if so it returns true
     {
-        if (tileset.GetTile(tileset.WorldToCell(Position)).name.EndsWith("0"))
+        if (tileset.GetColor(tileset.WorldToCell(Position)) == Color.gray)
         {
             return false;
+            
         }
         else
         {
             return true;
+            uManager.AddXP(67.0f);
         }
     }
 
-// 0 is gray, 1 is red, 2 is blue, 3 is green, 4 is orange
-    void placeTiles(Vector3[] positions, int[] tile)
+    // position has to be an array, that way multiple tiles can be placed instantly, 0 is gray, 1 is red, 2 is blue, 3 is green, 4 is orange
+    void placeTiles(Vector3[] positions, int color)
     {
-        var cellPosition = new Vector3Int[positions.Length];
-        var actualTiles = new TileBase[tile.Length];
-        print(positions.Length);
-        for (int i = 0; i < positions.Length; i++) 
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            tileset.SetTileFlags(tileset.WorldToCell(positions[i]), TileFlags.None);
+            switch (color)
             {
-                cellPosition[i] = tileset.WorldToCell(positions[i]);
-            print("BBBBB");
+                case 0:
+                    tileset.SetColor(tileset.WorldToCell(positions[i]), Color.gray);
+                    
+                    break;
+                case 1:
+                    tileset.SetColor(tileset.WorldToCell(positions[i]), Color.red);
+                    break;
+                case 2:
+                    tileset.SetColor(tileset.WorldToCell(positions[i]), Color.blue);
+                    break;
+                case 3:
+                    tileset.SetColor(tileset.WorldToCell(positions[i]), Color.green);
+                    break;
+                case 4:
+                    tileset.SetColor(tileset.WorldToCell(positions[i]), Color.yellow);
+                    break;
+            }
+            
+
             {
 
             }
-            }
-        print("CCCCC");
-        for (int i = 0; i < tile.Length; i++)
-            {
-            print("DDDDD");
-            var idk = Tile.Instantiate();
-                idk.name = tile[i].ToString();
-                idk.sprite = Resources.Load("Assets/Sprites/tempTileColours - Replace_" + tile[i].ToString()) as Sprite;
-            actualTiles[i] = idk;
-            }
-
-        tileset.SetTiles(cellPosition, actualTiles);
-    } 
+        }
+    }
 }
+  
